2741117d9ed996d5dda04e4a4ded63365cc9081b
Umairulisam <umairulislam09@gmail.com>
2025-07-09 18:17:15 +0500

    remove order online banner

--- src/pages/order-online/OrderOnline.tsx
+++ src/pages/order-online/OrderOnline.tsx
@@ -1,17 +1,9 @@
-import { OrderOnlineBanner } from "../../assets"
 import { Dessert, Salad } from "../../components"
 
 const OrderOnline = () => {
   return (
     <main className="py-6 bg-light">
       <div className="container">
-        {/* <img
-          src={OrderOnlineBanner}
-          alt="restaurant"
-          height="200px"
-          className="object-fit-cover rounded-4 w-100"
-        /> */}
-
         <Salad />
         <Dessert />
       </div>
